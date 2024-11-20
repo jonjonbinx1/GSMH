e4ea1475ea8fb8d84edc02af4fdb0fc4ef3b3a57
Jonjon Binks <johnbh16@gmail.com>
2024-11-19 22:38:17 -0500

    adding annotations and comments

--- PyTrends/pytrendsRequester.py
+++
@@ -1,8 +0,0 @@
-from pytrends.request import TrendReq
-pytrends = TrendReq(hl='en-US', tz=360)
-
-pytrends.build_payload(['depression', 'adhd'], cat=0, timeframe='today 5-y', geo='', gprop='')
-interest_over_time = pytrends.interest_over_time()
-print(interest_over_time['depression'].to_csv())
-
-
